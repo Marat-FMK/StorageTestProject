69183147b89c406efd4c29458ef8631d6b154fee
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-03-19 20:14:25 +0300

    Initial Commit

---
+++ StorageTestProject/StorageTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorageTestProjectApp.swift
+//  StorageTestProject
+//
+//  Created by Marat Fakhrizhanov on 19.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct StorageTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
