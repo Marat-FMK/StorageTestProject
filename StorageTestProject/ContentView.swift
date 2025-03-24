83874da243105caacb97c3e3f7d7d2c0bb784116
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-03-24 15:26:08 +0300

    app auth

--- StorageTestProject/ContentView.swift
+++ StorageTestProject/ContentView.swift
@@ -6,19 +6,59 @@
 //
 
 import SwiftUI
+//import Firebase
+import FirebaseAuth
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+extension ContentView {
+    enum Path {
+        case MainView
+        case RegistrationView
     }
 }
-
+struct ContentView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    @State private var path = NavigationPath()
+    
+    var body: some View {
+        
+        NavigationStack(path: $path) {
+            VStack {
+                TextField("Email", text: $email)
+                    .textFieldStyle(.roundedBorder)
+                
+                TextField("Password", text: $password)
+                    .textFieldStyle(.roundedBorder)
+                
+                Button {
+                    
+                    Auth.auth().signIn(withEmail: email, password: password ){ one,two in
+                        
+                        if two == nil {
+                            path.append(Path.MainView)
+                        }
+                    }
+                }label: {
+                    Text("LogIn")
+                }
+                
+                Button {
+                    path.append(Path.RegistrationView)
+                } label: {
+                    Text ("registration new accaunt")
+                }
+            }
+            .navigationDestination(for: Path.self) { value in
+                switch value {
+                    
+                case .MainView : MainView(path: $path)
+                case .RegistrationView : RegistrationView(path: $path)
+                }
+            }
+        }
+    }
+}
 #Preview {
     ContentView()
 }
