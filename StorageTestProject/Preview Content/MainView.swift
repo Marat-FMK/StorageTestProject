b60aaba77647fab1e361972ef0b9091ea2a47c8b
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-03-24 15:25:52 +0300

    app auth

---
+++ StorageTestProject/Preview Content/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginView.swift
+//  StorageTestProject
+//
+//  Created by Marat Fakhrizhanov on 23.03.2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("In app")
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ StorageTestProject/Preview Content/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  StorageTestProject
+//
+//  Created by Marat Fakhrizhanov on 24.03.2025.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
