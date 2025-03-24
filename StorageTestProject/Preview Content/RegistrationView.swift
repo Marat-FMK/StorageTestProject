14e2994386cb5dc7e2d98931f9a14776c8ede748
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-03-24 15:26:20 +0300

    app auth

--- StorageTestProject/Preview Content/RegistrationView.swift
+++ StorageTestProject/Preview Content/RegistrationView.swift
@@ -6,13 +6,41 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct RegistrationView: View {
+    @Binding var path: NavigationPath
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+        VStack{
+            TextField("Email", text: $email)
+                .textFieldStyle(.roundedBorder)
+            
+            TextField("Password", text: $password)
+                .textFieldStyle(.roundedBorder)
+            
+            Button {
+                Auth.auth().createUser(withEmail: email, password: password) {_, error in
+                    
+                    if error == nil {
+                        print(" new user active ")
+                    } else {
+                        print(" error / no new user")
+                    }
+                    
+                }
+            } label: {
+                Text("Registrantion")
+            }
+        }
+        
     }
 }
 
 #Preview {
-    RegistrationView()
+    RegistrationView(path: .constant(NavigationPath()))
 }
